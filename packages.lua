5cce8b673af1312e5626d5bb0563e833a5884e36
march012847 <tur830106@tkt.ac.th>
2025-09-06 07:57:08 +0700

    why

---
+++ packages.lua
@@ -0,0 +1,11 @@
+local packages = {
+    Package1 = {
+        version = "0.0.1",
+        location = "ServerScriptService",
+        url = "placeholder"
+    },
+    Package2 = {
+        version = "0.0.1",
+        location = "ReplicatedStorage"
+    }
+}
